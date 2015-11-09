e109cbd99e22c418324bf2820cbb8d18aee5a4a2
Fiona Gruell <Fiona@pc01.pool.tp1.uni-duesseldorf.de>
2015-11-09 12:27:39 +0100

    hw2

--- logmap.cxx
+++ logmap.cxx
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main(){
+int main(void){
 	double x0 = 0.5;
 	double x;
+	ofstream out ("output.dat");//creates file to store data in 
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+	double v[Nend - Nskip]; // Array to store the data in 
+	cout << "Berechnung der Daten" << endl;
 
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
@@ -14,10 +18,16 @@ int main(){
 		   x = r*x*(1-x);
 	   for(int i=Nskip+1; i <= Nend; i++){
 	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
+	   		   v[i-(Nskip+1)]=x; // Vektor bei festem r von Step 100 bis 200
    	   }
+	
+   	   cout << "Schreiben der Daten in Datei bei r="<<r << endl;
+	for(int i=Nskip+1; i<=Nend ; i++)   {
+   	   out << r << "\t =r"<<"\t v["<< i-(Nskip)<<"]\t = \t"<< v[i-Nskip-1]<< endl; //Ausgabe in Datei bei festem r
+	}
 	}
 
-
+out.close();
+cout <<"Schreiben beendet"<< endl;
 	return 0;
 }
